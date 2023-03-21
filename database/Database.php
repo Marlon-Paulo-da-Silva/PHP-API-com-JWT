c4b7a11ff1e13eb7dd54d3a8bc43ab1da604b6a0
Marlon Paulo <marlon.paulo.silva@outlook.com>
2023-03-21 11:43:16 -0300

    Adicionando conexão com database

---
+++ database/Database.php
@@ -0,0 +1,46 @@
+<?php
+  class Database{
+    private $host = "localhost";
+    private $database = "root";
+    private $user = "";
+    private $password = "php_api_jwt";
+
+    private $mysqli = "";
+    private $result = array();
+    private $conn = false;
+
+    // conectando ao banco de dados usando contruct
+    public function __construct()
+    {
+      if(!$this->conn){
+        $this->mysqli = new mysqli($this->host, $this->user, $this->password, $this->database);
+        $this->conn = true;
+
+        if($this->mysqli->connect_error){
+          array_push($this->result, $this->mysqli->connect_error);
+          return false;
+        }
+
+      } else {
+        return true;
+      }
+    }
+
+    public function getResult(){
+      $val = $this->result;
+      $this->result =  array();
+      return $val;
+    }
+
+    public function __destruct()
+    {
+      if($this->conn){
+        if($this->mysqli->close()){
+          $this->conn = false;
+        }
+      } else {
+        return false;
+      }
+    }
+  }
+?>
